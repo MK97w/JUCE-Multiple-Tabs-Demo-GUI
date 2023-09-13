3c5c7e8ee7ee627d9f1c31087bfa8fb7cacb6063
MERT KABUKÇUOĞLU <kabukcuoglumert@gmail.com>
2023-09-14 02:30:41 +0300

    Every tab can draw it's related jpg and it creates some sort of "changing page" feeling. Must experiment with sizing and LookAndFeel of button tab.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -26,5 +26,5 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    
+    tabs.setBounds(getLocalBounds());
 }
--- Source/SeasonSelectorTab.h
+++ Source/SeasonSelectorTab.h
@@ -22,21 +22,53 @@ namespace juce
             //auto colour = findColour (ResizableWindow::backgroundColourId);
             auto colour = Colours::transparentBlack;
             
-            addTab ("Winter",     colour, new WinterPage(),                                 true);
+            addTab ("Winter",     colour, new WinterPage(),                     true);
             addTab ("Spring",     colour, new SpringPage(),                                 true);
             addTab ("Summer",     colour, new SummerPage(),                                 true);
             addTab ("Autumn",     colour, new AutumnPage(),                                 true);
             
             setOutline(0);
             //setSize(230, 300);
-            setBounds(525, 0, 230, 300);
-            //setLookAndFeel(<#LookAndFeel *newLookAndFeel#>)
+            //setBounds(523, 0, 232, 300);
+            //setBounds(getLocalBounds());
+            //setLookAndFeel(LookAndFeel *newLookAndFeel)
         };
     private:
-        struct WinterPage: public Component{};
-        struct SpringPage: public Component{};
-        struct SummerPage: public Component{};
-        struct AutumnPage: public Component{};
+
+        struct WinterPage: public Component
+        {
+            WinterPage() = default;
+            void paint(Graphics& g) override
+            {
+                g.drawImage(ImageCache::getFromMemory(BinaryData::winter_jpg, BinaryData::winter_jpgSize),getLocalBounds().toFloat());
+            }
+        };
+        
+        struct SpringPage: public Component
+        {
+            SpringPage() = default;
+            void paint(Graphics& g) override
+            {
+                g.drawImage(ImageCache::getFromMemory(BinaryData::spring_jpg, BinaryData::spring_jpgSize),getLocalBounds().toFloat());
+            }
+        };
+        
+        struct SummerPage: public Component
+        {
+            SummerPage() = default;
+            void paint(Graphics& g) override
+            {
+                g.drawImage(ImageCache::getFromMemory(BinaryData::summer_jpg, BinaryData::summer_jpgSize),getLocalBounds().toFloat());
+            }
+        };
+        struct AutumnPage: public Component
+        {
+            AutumnPage() = default;
+            void paint(Graphics& g) override
+            {
+                g.drawImage(ImageCache::getFromMemory(BinaryData::autumn_jpg, BinaryData::autumn_jpgSize),getLocalBounds().toFloat());
+            }
+        };
         
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SeasonSelectorTab);
     };
